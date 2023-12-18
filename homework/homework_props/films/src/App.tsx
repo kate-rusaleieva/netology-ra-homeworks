b66d3566bc8e94a13b7b86dd51efda890acd9c82
kate-rusaleieva <kate-rusaleieva15@mail.ru>
2023-12-18 21:37:38 +0700

    Домашнее задание "Props"

--- homework/homework_props/films/src/App.tsx
+++ homework/homework_props/films/src/App.tsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Stars from './components/Stars';
+
+
+const films = [{
+  id: 'film-01',
+  poster: './assets/superman.jpg',
+  title: 'Супермен',
+  stars: 4,
+  price: 1299,
+  type: 'Экшн',
+  color: 'orange',
+  isFavorite: true
+}];
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="card u-clearfix">
+
+          <div className="card-media">
+            <img src={films[0].poster} alt="" className="card-media-img" />
+          </div>
+
+          <div className="card-body">
+            <h2 className="card-body-heading">{films[0].title}</h2>
+            <div className="card-body-options">
+              <Favorite isChecked={films[0].isFavorite} />
+              <Share />
+            </div>
+            <Stars count={films[0].stars} />
+          </div>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
