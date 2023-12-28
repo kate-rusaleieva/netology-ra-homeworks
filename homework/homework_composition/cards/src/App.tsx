220bbeadcdff64385a988e5d9b47547b5db5d5b1
kate-rusaleieva <kate-rusaleieva15@mail.ru>
2023-12-28 21:35:17 +0700

    Домашнее задание "Карточки"

--- homework/homework_composition/cards/src/App.tsx
+++ homework/homework_composition/cards/src/App.tsx
@@ -3,12 +3,14 @@ import Card from './components/Card'
 
 const cards = [
   {
+    id: 1,
     title: "Card title",
     text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
     img: <svg className="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>,
     button: "Go somewhere"
   },
   {
+    id: 2,
     title: "Special title",
     text: "With supporting text below as a natural lead-in to additional content",
     button: "Go somewhere"
@@ -23,7 +25,7 @@ function App() {
         {
           cards.map((card, i) => {
             return (
-              <Card {...card} children={card.img} />
+              <Card {...card} children={card.img} key={card.id + i} />
             );
           })
         }
